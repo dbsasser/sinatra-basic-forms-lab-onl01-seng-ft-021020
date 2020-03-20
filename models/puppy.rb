6e5bc19de820b0f80262231a18bdbac045f8c5d1
Daniel Sasser <danielbsasser@gmail.com>
2020-03-20 02:23:24 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -4,10 +4,10 @@ class Puppy
   attr_accessor :name, :age
   attr_reader :breed
 
-  def initialize(name, breed, age)
-    @name = name
-    @breed = breed
-    @age = age
+  def initialize(hash)
+    @name = name:
+    @breed = breed:
+    @age = age:
   end
 
 end
